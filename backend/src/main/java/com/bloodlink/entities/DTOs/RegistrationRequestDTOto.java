96bd95c83f93afd7a5ce0905892bbc5173cb5b26
Rameev Timur <stt1xx@mail.ru>
2025-03-18 02:32:20 +0300

    added creation date ti RegistrationRequestDTO

--- backend/src/main/java/com/bloodlink/entities/DTOs/RegistrationRequestDTOto.java
+++ backend/src/main/java/com/bloodlink/entities/DTOs/RegistrationRequestDTOto.java
@@ -4,6 +4,8 @@ import com.bloodlink.entities.RegistrationRequest;
 import com.bloodlink.entities.enums.Role;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class RegistrationRequestDTOto {
 
@@ -17,22 +19,25 @@ public class RegistrationRequestDTOto {
 
     private Role role;
 
+    private LocalDateTime createdAt;
+
     private OrganizationDTOto organization;
 
     public RegistrationRequestDTOto(Long id, String name, String surname, String email, Role role,
-                                    OrganizationDTOto organization) {
+                                    OrganizationDTOto organization, LocalDateTime createdAt) {
         this.id = id;
         this.name = name;
         this.surname = surname;
         this.email = email;
         this.role = role;
         this.organization = organization;
+        this.createdAt = createdAt;
     }
 
     public static RegistrationRequestDTOto convert(RegistrationRequest entity) {
         return new RegistrationRequestDTOto(entity.getId(), entity.getName(), entity.getSurname(), entity.getEmail(),
                 entity.getRole(), OrganizationDTOto.convert(entity.getBloodBank() == null ?
-                entity.getInstitution() : entity.getBloodBank()));
+                entity.getInstitution() : entity.getBloodBank()), entity.getCreatedAt());
     }
 
 
