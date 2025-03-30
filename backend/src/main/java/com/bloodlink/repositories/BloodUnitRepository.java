ec38681396eadd412147d1d9bc3c7e11c5476aec
Rameev Timur <stt1xx@mail.ru>
2025-03-31 02:45:04 +0300

    add shedule func

--- backend/src/main/java/com/bloodlink/repositories/BloodUnitRepository.java
+++ backend/src/main/java/com/bloodlink/repositories/BloodUnitRepository.java
@@ -1,16 +1,21 @@
 package com.bloodlink.repositories;
 
 import com.bloodlink.entities.BloodUnit;
-import com.bloodlink.entities.Organization;
-import com.bloodlink.entities.enums.BloodGroup;
-import com.bloodlink.entities.enums.RhFactor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface BloodUnitRepository extends JpaRepository<BloodUnit, Long>, JpaSpecificationExecutor<BloodUnit> {
+
+    @Query("SELECT b FROM BloodUnit b WHERE b.expirationDate < CURRENT_DATE")
+    List<BloodUnit> findExpiredUnits();
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM BloodUnit b WHERE b.expirationDate < CURRENT_DATE")
+    int deleteExpiredUnits();
 }
